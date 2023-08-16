f23873e25e172c9284bd5f1cd9bc081944618e84
kenzo <55468155+fakeemotions@users.noreply.github.com>
2023-08-16 17:04:41 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,6 @@ function openURL(type) {
     let url = 'about:blank'
 
     switch (type) {
-        case 'discord':
-            navigator.clipboard.writeText("discord");
-            break
-        case 'instagram':
-            window.open("https://www.instagram.com/wwwwwwwwwwwwwwwwwwwkkwwwwwwwww/")
-            break
         case 'spotify':
             window.open("https://open.spotify.com/user/lvia5ekfy3co0n5rrbw218i9u")
             break
@@ -15,10 +9,4 @@ function openURL(type) {
             window.open("https://last.fm/user/kenzovmp")
             break
     }
-}
-
-function popup() {
-    console.log("Popup")
-    var popup = document.getElementById('popup1')
-    popup.classList.toggle('anim')
 }
